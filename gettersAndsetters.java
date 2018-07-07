03fd9b4655b3313bde4626eb0461cbb0f11ae39b
Light <35003912+Yuuurii@users.noreply.github.com>
2018-07-08 00:03:18 +0800

    Update gettersAndsetters.java

--- gettersAndsetters.java
+++ gettersAndsetters.java
@@ -1,14 +1,14 @@
 
 public class gettersAndsetters {
 	
-	private String data;
-	
+	private String data;// eto yung string o yung variable na lalagyan ng value
+	//String parameter ang input value
 	public void setData(String parameter) {
 		data = parameter;
 	}
 	
 	public String getData() {
-		return data;
+		return data; // return data yung output.
 	}
 	
 	public void showHowifitworks(String parameter) {
